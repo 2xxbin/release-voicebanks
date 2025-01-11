aac3e7764e6d9d8d81ba1444c64043f90d2f9fb0
2xxbin <2xxbin1208@gmail.com>
2025-01-12 04:06:10 +0900

    add imagemin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import imageminWebp from 'imagemin-webp';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import imageminAvif from 'imagemin-avif';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import imageminMozjpeg from 'imagemin-mozjpeg';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		viteImagemin({
+			plugins: {
+				png: imageminPngquant(),
+				jpg: imageminMozjpeg()
+			},
+			makeWebp: {
+				plugins: {
+					jpg: imageminWebp(),
+					png: imageminWebp()
+				}
+			},
+			makeAvif: {
+				plugins: {
+					jpg: imageminAvif(),
+					png: imageminAvif()
+				}
+			}
+		}),
+		sveltekit(),
+		viteCompression({ algorithm: 'gzip', ext: 'gz' })
+	],
 	optimizeDeps: {
 		include: ['@splidejs/splide', '@splidejs/svelte-splide']
 	}
